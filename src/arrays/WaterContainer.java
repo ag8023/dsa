3e9bbc38941f578922865354d216c82ababded75
ag8023 <aasavari@gmail.com>
2021-02-15 23:59:42 -0800

    day5

--- src/arrays/WaterContainer.java
+++ src/arrays/WaterContainer.java
@@ -1,8 +1,16 @@
-package src.arrays;
+package arrays;
 
 public class WaterContainer {
     public int maxArea(int[] height) {
 
-
+        int maxArea = 0;
+        for(int i = 0; i < height.length; i++)
+        {
+            for(int j = i+1; j < height.length; j++)
+            {
+                maxArea = Math.max(maxArea, Math.min(height[i], height[j]) *(j-i));
+            }
+        }
+     return maxArea;
     }
 }
